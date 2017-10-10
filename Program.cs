65e34873c6d78c4cf7136c292a0b7186893b787f
XxJadenxX <32143231+XxJadenxX@users.noreply.github.com>
2017-10-10 14:00:03 -0600

    a fix

--- Program.cs
+++ Program.cs
@@ -4,13 +4,16 @@ namespace HelloWorld
 {
     class MainClass
     {
-        public static void Main(string[] args)
-        {
+        public static
             Console.WriteLine("Write 2 numbers");
-            float number1 = float.Parse(Console.ReadLine());
-            float number2 = float.Parse(Console.ReadLine());
-            Console.WriteLine(number1 + number2);
+        float number1 = float.Parse(Console.ReadLine());
+        float number2 = float.Parse(Console.ReadLine());
+        Console.WriteLine(AddTwoNumbers(number1, number2));
 
         }
+    public static float AddTwoNumbers(float n1, float n2)
+    {
+        float result = n1 + n2;
+
+        return result;
     }
-}
