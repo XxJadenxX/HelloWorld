3d079b852e2065b05942025374f1251c2c295f55
XxJadenxX <32143231+XxJadenxX@users.noreply.github.com>
2017-10-11 13:49:54 -0600

    If statements adjusted

--- Program.cs
+++ Program.cs
@@ -4,16 +4,32 @@ namespace HelloWorld
 {
     class MainClass
     {
-        public static
+        public static void Main(string[] args)
+        {
             Console.WriteLine("Write 2 numbers");
-        float number1 = float.Parse(Console.ReadLine());
-        float number2 = float.Parse(Console.ReadLine());
-        Console.WriteLine(AddTwoNumbers(number1, number2));
+            float number1 = float.Parse(Console.ReadLine());
+            float number2 = float.Parse(Console.ReadLine());
 
+            Console.WriteLine("Select an option\n1.-Add\n2.-Substract\n3.-Multiply\n4.-Divide");
+
+            int option = int.Parse(Console.ReadLine());
+            EvaluateOption(option, number1, number2);
+
+            Console.WriteLine(option);
         }
-    public static float AddTwoNumbers(float n1, float n2)
-    {
-        float result = n1 + n2;
 
-        return result;
+        public static float AddTwoNumbers(float n1, float n2)
+        {
+            float result = n1 + n2;
+
+            return result;
+        }
+        public static void EvaluateOption(int option, float n1, float n2)
+        {
+            if(option == 1){
+                 
+            }
+            Console.WriteLine(AddTwoNumbers(n1, n2));
+        }
     }
+}
